9c69561184e412cd24c38292b29d6c7d9b1f0f49
josezanle <jose.rios.lm@gmail.com>
2021-01-26 20:55:56 -0300

    add de proyectos reales

--- components/galery/index.js
+++ components/galery/index.js
@@ -4,26 +4,24 @@ const webs = [
   {
     id: 1,
     image_url:
-      "https://images.unsplash.com/photo-1516633630673-67bbad747022?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+      "https://firebasestorage.googleapis.com/v0/b/sql-demos-3f601.appspot.com/o/image.jpg?alt=media&token=0ee2a779-1a7d-4a2f-9365-ee2aecc05aeb",
     description:
-      "esta es una descripcion acerca de la pagina realizada, click abajo para ir a la web.",
-    url: "",
+      "Globy, es una empresa Mexicana de taxi al estilo Uber, en proceso de formacion.",
+    url: "https://www.globy.dancotll.com/",
   },
   {
     id: 2,
     image_url:
-      "https://images.unsplash.com/photo-1520520731457-9283dd14aa66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-    description:
-      "esta es una descripcion acerca de la pagina realizada, click abajo para ir a la web.",
-    url: "",
+      "https://firebasestorage.googleapis.com/v0/b/sql-demos-3f601.appspot.com/o/zanle-group-portada.jpg?alt=media&token=329e26d9-d08b-454a-a181-669e6cc61f13",
+    description: "Zanle Group, web para freelance, dirigida a Latino America",
+    url: "https://zanle-group.vercel.app/",
   },
   {
     id: 3,
     image_url:
-      "https://images.unsplash.com/photo-1496046744122-2328018d60b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1364&q=80",
-    description:
-      "esta es una descripcion acerca de la pagina realizada, click abajo para ir a la web.",
-    url: "",
+      "https://firebasestorage.googleapis.com/v0/b/sql-demos-3f601.appspot.com/o/portada-portfolio.jpg?alt=media&token=fcc58a63-1720-4043-ad94-3db6ed9cda42",
+    description: "El primer portfolio web que hice, orientado al diseño ux/ui.",
+    url: "https://jose-rios.netlify.app/#/",
   },
 ];
 
@@ -38,8 +36,11 @@ const Galery = () => {
             <img src={image_url} alt="" />
           </div>
           <div className="mid">{description}</div>
+
           <div className="bottom">
-            <Button url={url} />
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              Visitar
+            </a>
           </div>
         </li>
       ))}
@@ -63,8 +64,8 @@ const Galery = () => {
 
           // ===========================================================================
           .card {
-            width: 250px;
-            height: 300px;
+            width: 300px;
+            height: 400px;
             border: 0.5em solid #ebebeb;
             border-radius: 1em;
             padding: 1em;
@@ -75,9 +76,6 @@ const Galery = () => {
             flex-flow: column;
           }
 
-          // h4 {
-          //   color: silver;
-          // }
           // ======================================================================================
           .top {
             flex: 5;
@@ -85,22 +83,35 @@ const Galery = () => {
             height: 100%;
           }
           img {
-            width: 100%;
-            height: 100%;
+            width: 252px;
+            height: 196px;
             border-radius: 0.5em;
+            object-fit: cover;
           }
           // ======================================================================================
           .mid {
             flex: 4;
             width: 100%;
             height: 100%;
-            margin: 5px 0;
+            margin: 0.5em 0;
           }
           // =======================================================================================
           .bottom {
             flex: 1;
             width: 100%;
             height: 100%;
+            background: #f8ac30;
+            border-radius: 0.5em;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+          a {
+            color: black;
+          }
+          a:hover {
+            color: white;
+            cursor: pointer;
           }
         `}
       </style>
